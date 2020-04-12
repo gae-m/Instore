32b39988a88f3552a5358ac686277816c5b88e5d
Gaetano <gaetano.martedi1@studenti.unicampania.it>
2020-04-12 19:24:29 +0200

    prova

--- app/src/main/java/com/example/instore/home/HomeFragment.kt
+++ app/src/main/java/com/example/instore/home/HomeFragment.kt
@@ -1,11 +1,11 @@
-package com.example.instore
+package com.example.instore.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import com.example.instore.R
 
 /**
  * A simple [Fragment] subclass.
