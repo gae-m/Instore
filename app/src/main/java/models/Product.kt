eab34deb059d256c5c0385f2796739bef14edce7
biagi <biagio.salzillo@studenti.unicampania.it>
2020-04-15 17:22:05 +0200

    1) Project Start

--- app/src/main/java/models/Product.kt
+++ app/src/main/java/models/Product.kt
@@ -6,7 +6,6 @@ class Product {
     var taglia: String = ""
     var prezzo: Double = 0.0
     var colore: String = ""
-    var quant_disp: Int = 0
     var sesso: String = ""
     var anno: Int = 0
     var imgUrl: String = ""
